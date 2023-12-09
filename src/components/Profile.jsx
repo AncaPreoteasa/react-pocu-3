14b01675e623f7d4880c1f39abbd6cb368e18df8
Anca Preoteasa <114996344+AncaPreoteasa@users.noreply.github.com>
2023-12-09 17:53:53 +0200

    Updated Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,28 +1,44 @@
 // import styles from "./Profile.module.css"
+import { Email } from "@mui/icons-material";
 import styles from "./Forms.module.css";
+import { useAuthContext } from "../features/Auth/AuthContext";
 
 export function Profile() {
+  const { user } = useAuthContext();
+
   return (
     <>
       <h1>Profile component</h1>
-      <form className="brandForm">
-        <label htmlFor="email"></label>
-        <input type="email" name="email" id="email">
-          Email
-        </input>
-        <label htmlFor="password"></label>{" "}
-        <input type="password" name="password" id="password">
-          Password
-        </input>
-        <label htmlFor="firstName"></label>{" "}
-        <input type="text" name="firstName" id="firstName">
-          First Name
-        </input>
-        <label htmlFor="lastName"></label>{" "}
-        <input type="text" name="lastName" id="lastName">
-          Last Name
-        </input>
-        <button type="submit">Submit</button>
+      <form className="brandForm" action="/upodaskdlakjl.php" method="post">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          // defaultValue={user.email}
+        ></input>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          // defaultValue={user.password}
+        ></input>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          name="firstName"
+          id="firstName"
+          // defaultValue={user.firstName}
+        ></input>
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          // defaultValue={user.lastName}
+        ></input>
+        <input type="submit"></input>
       </form>
     </>
   );
