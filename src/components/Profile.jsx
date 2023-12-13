01055d2f5a43dec4b5c84697a1f3160dc095e13c
Anca Preoteasa <114996344+AncaPreoteasa@users.noreply.github.com>
2023-12-13 21:11:51 +0200

    Validate field for profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,23 @@
 import styles from "./Profile.module.css";
 import { useAuthContext } from "../features/Auth/AuthContext";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { object, ref, string } from "yup";
+import { PasswordInput } from "./PasswordInput/PasswordInput";
+
+const schema = object({
+  email: string()
+    .email("The email address is not valid")
+    .required("Please provide an email address"),
+  password: string()
+    .required("Please type a password")
+    .min(4, "The password needs to be at least 4 characters long"),
+  retypePassword: string()
+    .required("Please type your password again.")
+    .oneOf([ref("password")], "The two passwords do not match"),
+  firstName: string().required("Please tell us your first name"),
+  lastName: string().required("Please tell us your last name"),
+});
 
 export function Profile() {
   const { user, updateUserProfile } = useAuthContext();
@@ -9,7 +26,9 @@ export function Profile() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ resolver: yupResolver(profileSchema) });
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const onSubmit = (userData) => {
     updateUserProfile(userData);
@@ -25,16 +44,38 @@ export function Profile() {
           name="email"
           id="email"
           defaultValue={user.email}
+          readOnly
           {...register("email")}
         ></input>
+        {errors.email && (
+          <p className="secondColumn fieldError">{errors.email.message}</p>
+        )}
+
         <label htmlFor="password">Password 🗝️</label>
         <input
           type="password"
           name="password"
           id="password"
-          defaultValue={user.password}
           {...register("password")}
         ></input>
+        {errors.password && (
+          <p className="secondColumn fieldError">{errors.password.message}</p>
+        )}
+
+        <label className={styles.label} htmlFor="retypePassword">
+          Retype Password
+        </label>
+        <PasswordInput
+          className={styles.input}
+          name="retypePassword"
+          {...register("retypePassword")}
+        />
+        {errors.retypePassword && (
+          <p className="secondColumn fieldError">
+            {errors.retypePassword.message}
+          </p>
+        )}
+
         <label htmlFor="firstName">First Name</label>
         <input
           type="text"
@@ -43,6 +84,10 @@ export function Profile() {
           defaultValue={user.firstName}
           {...register("firstName")}
         ></input>
+        {errors.firstName && (
+          <p className="secondColumn fieldError">{errors.firstName.message}</p>
+        )}
+
         <label htmlFor="lastName">Last Name</label>
         <input
           type="text"
@@ -51,6 +96,10 @@ export function Profile() {
           defaultValue={user.lastName}
           {...register("lastName")}
         ></input>
+        {errors.lastName && (
+          <p className="secondColumn fieldError">{errors.lastName.message}</p>
+        )}
+
         <button type="submit">Submit changes</button>
       </form>
     </div>
